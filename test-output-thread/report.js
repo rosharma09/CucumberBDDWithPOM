3fe7e2aa142dcba181b4013ae66010d986d10513
rosharma09 <56192256+rosharma09@users.noreply.github.com>
2021-01-04 21:43:15 +0530

    Cucumber BDD project using JUnit

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,168 +1,146 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "ebbc34ec-3cbd-4993-953e-2f8ce1c80404",
-    "feature": "Login page features",
-    "scenario": "In case the user forgets the password, the user can recall the password using the",
-    "start": 1609767033681,
-    "group": 18,
-    "content": "",
-    "tags": "",
-    "end": 1609767052680,
-    "className": "passed"
-  },
-  {
-    "id": "1c569dc7-35e8-4a30-8cc4-9e39d36466ed",
-    "feature": "Search item feature",
-    "scenario": "User searches for the following item using the search field",
-    "start": 1609767040916,
-    "group": 19,
-    "content": "",
-    "tags": "",
-    "end": 1609767065422,
-    "className": "passed"
-  },
-  {
-    "id": "4055c107-d999-4033-beab-25ed4c08b9c8",
-    "feature": "Login page features",
-    "scenario": "In order to search an item on ecom application, the search bar should be displayed",
-    "start": 1609767073106,
-    "group": 18,
-    "content": "",
-    "tags": "",
-    "end": 1609767090116,
-    "className": "passed"
-  },
-  {
-    "id": "cd47218c-1752-40fa-a8a7-4ffea3625031",
+    "id": "325389df-7531-490d-b301-d280a38033dd",
     "feature": "Home page Feature",
     "scenario": "The user can click on the various options displayed on the account section",
-    "start": 1609767095481,
+    "start": 1609774133770,
     "group": 17,
     "content": "",
     "tags": "",
-    "end": 1609767112030,
+    "end": 1609774154243,
     "className": "passed"
   },
   {
-    "id": "c45c0353-12d2-4c93-bbaf-d4aee4054f46",
-    "feature": "Home page Feature",
-    "scenario": "If the user is logged in to the application,",
-    "start": 1609767038529,
-    "group": 17,
-    "content": "",
-    "tags": "",
-    "end": 1609767058948,
-    "className": "passed"
-  },
-  {
-    "id": "8d30b77a-5516-4f2b-8a58-65ef3e61f025",
-    "feature": "Home page Feature",
-    "scenario": "The user can click on the various options displayed on the account section",
-    "start": 1609767112038,
-    "group": 17,
-    "content": "",
-    "tags": "",
-    "end": 1609767128008,
-    "className": "passed"
-  },
-  {
-    "id": "19128ff8-d641-4a52-b25b-e06443821876",
-    "feature": "Home page Feature",
-    "scenario": "The user can click on the various options displayed on the account section",
-    "start": 1609767078559,
-    "group": 17,
-    "content": "",
-    "tags": "",
-    "end": 1609767095473,
-    "className": "passed"
-  },
-  {
-    "id": "31dcd061-e9cf-417d-be09-40488a395d1a",
-    "feature": "Login page features",
-    "scenario": "Under the my account section in the footer, the various option are displayed",
-    "start": 1609767090125,
-    "group": 18,
-    "content": "",
-    "tags": "",
-    "end": 1609767105273,
-    "className": "passed"
-  },
-  {
-    "id": "3ed0febf-62ad-4275-9d16-758946b5d8bf",
-    "feature": "Login page features",
-    "scenario": "To check the page title is displayed correctly",
-    "start": 1609767013677,
-    "group": 18,
-    "content": "",
-    "tags": "",
-    "end": 1609767033595,
-    "className": "passed"
-  },
-  {
-    "id": "21cf3f6e-d209-4928-b55d-6f1a4cc5ffb0",
-    "feature": "Home page Feature",
-    "scenario": "If a user logs into the application using valid credentials",
-    "start": 1609767013584,
-    "group": 17,
-    "content": "",
-    "tags": "",
-    "end": 1609767038493,
-    "className": "passed"
-  },
-  {
-    "id": "1fd4f9c1-c942-46f3-9dbe-c5045091ed4e",
-    "feature": "Search item feature",
-    "scenario": "User searches for the following item using the search field",
-    "start": 1609767013586,
-    "group": 19,
-    "content": "",
-    "tags": "",
-    "end": 1609767040886,
-    "className": "passed"
-  },
-  {
-    "id": "26b53882-a60a-4acd-bb09-1caf9606c66c",
-    "feature": "Home page Feature",
-    "scenario": "The user can click on the various options displayed on the account section",
-    "start": 1609767058959,
-    "group": 17,
-    "content": "",
-    "tags": "",
-    "end": 1609767078551,
-    "className": "passed"
-  },
-  {
-    "id": "b18a3ce6-f7d4-43ab-8f38-ce96a3f1af21",
-    "feature": "Home page Feature",
-    "scenario": "The user can click on the various options displayed on the account section",
-    "start": 1609767128022,
-    "group": 17,
-    "content": "",
-    "tags": "",
-    "end": 1609767145104,
-    "className": "passed"
-  },
-  {
-    "id": "6cfa611c-cf9f-4101-a8f2-c360b057d662",
-    "feature": "Login page features",
-    "scenario": "To check that the user is able to log into the application using the valid credentials",
-    "start": 1609767052705,
-    "group": 18,
-    "content": "",
-    "tags": "",
-    "end": 1609767073085,
-    "className": "passed"
-  },
-  {
-    "id": "d221fc16-0184-41c2-b946-451cecda30ac",
+    "id": "22a23db1-c997-4917-9852-a160580ff7bd",
     "feature": "Home page Feature",
     "scenario": "Once logged into the system, the uesr header info is displayed to the user Ex: Sign out and the user name",
-    "start": 1609767145111,
+    "start": 1609774154249,
     "group": 17,
     "content": "",
     "tags": "",
-    "end": 1609767168490,
+    "end": 1609774185477,
+    "className": "passed"
+  },
+  {
+    "id": "810d771b-7f3f-48c9-ac64-f67edc6c2be9",
+    "feature": "Home page Feature",
+    "scenario": "The user can click on the various options displayed on the account section",
+    "start": 1609774090020,
+    "group": 17,
+    "content": "",
+    "tags": "",
+    "end": 1609774116735,
+    "className": "passed"
+  },
+  {
+    "id": "39890cf3-9c03-42eb-bb93-a463adfcfeb4",
+    "feature": "Login page features",
+    "scenario": "Under the my account section in the footer, the various option are displayed",
+    "start": 1609774077083,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1609774099896,
+    "className": "passed"
+  },
+  {
+    "id": "806976b4-cdcd-4986-9c22-5efc3c8a1867",
+    "feature": "Home page Feature",
+    "scenario": "The user can click on the various options displayed on the account section",
+    "start": 1609774027641,
+    "group": 17,
+    "content": "",
+    "tags": "",
+    "end": 1609774056780,
+    "className": "passed"
+  },
+  {
+    "id": "063ec1d6-4bda-4ae5-b6e6-b77385bc35be",
+    "feature": "Home page Feature",
+    "scenario": "The user can click on the various options displayed on the account section",
+    "start": 1609774116747,
+    "group": 17,
+    "content": "",
+    "tags": "",
+    "end": 1609774133761,
+    "className": "passed"
+  },
+  {
+    "id": "f991b334-788a-4e88-b1dc-502f1d18ebd5",
+    "feature": "Login page features",
+    "scenario": "In order to search an item on ecom application, the search bar should be displayed",
+    "start": 1609774048524,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1609774077072,
+    "className": "passed"
+  },
+  {
+    "id": "66eef136-63a6-47ed-b543-2b08023b497b",
+    "feature": "Login page features",
+    "scenario": "To check that the user is able to log into the application using the valid credentials",
+    "start": 1609774017613,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1609774048514,
+    "className": "failed"
+  },
+  {
+    "id": "0eb29a09-08d5-45c1-8a48-1b3f1aebb17e",
+    "feature": "Home page Feature",
+    "scenario": "If the user is logged in to the application,",
+    "start": 1609773996817,
+    "group": 17,
+    "content": "",
+    "tags": "",
+    "end": 1609774027633,
+    "className": "passed"
+  },
+  {
+    "id": "de6f371b-ae13-4f8f-ba11-fbc1caf8eeae",
+    "feature": "Home page Feature",
+    "scenario": "If a user logs into the application using valid credentials",
+    "start": 1609773961513,
+    "group": 17,
+    "content": "",
+    "tags": "",
+    "end": 1609773996800,
+    "className": "passed"
+  },
+  {
+    "id": "66007bb1-a473-422b-a8d3-1c1bc8bba844",
+    "feature": "Login page features",
+    "scenario": "To check the page title is displayed correctly",
+    "start": 1609773961515,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1609773993242,
+    "className": "passed"
+  },
+  {
+    "id": "3118ff2e-bfb9-4f99-bbc1-1b17020ca6f1",
+    "feature": "Home page Feature",
+    "scenario": "The user can click on the various options displayed on the account section",
+    "start": 1609774056787,
+    "group": 17,
+    "content": "",
+    "tags": "",
+    "end": 1609774090003,
+    "className": "passed"
+  },
+  {
+    "id": "d8ca9f72-63e1-438d-8e73-90cfb70f7b66",
+    "feature": "Login page features",
+    "scenario": "In case the user forgets the password, the user can recall the password using the",
+    "start": 1609773993329,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1609774017594,
     "className": "passed"
   }
 ]);
@@ -174,10 +152,6 @@ CucumberHTML.timelineGroups.pushArray([
   {
     "id": 18,
     "content": "Thread[pool-1-thread-2,5,main]"
-  },
-  {
-    "id": 19,
-    "content": "Thread[pool-1-thread-3,5,main]"
   }
 ]);
 });
